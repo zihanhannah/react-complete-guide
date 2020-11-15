2cdbd8d247f9ba04b79db1d1d94afb152b761af0
zihan zhao <zihanhannah@gmail.com>
2020-11-14 22:41:09 -0600

    using props.children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,12 @@ import React from 'react';
 
 const person = (props) => {
 // return <p> I am a Person and I am {Math.floor(Math.random()*30)} years old!</p>
-return <p> I am {props.name} and I am {props.age} years old</p>
+return (
+<div>
+<p> I am {props.name} and I am {props.age} years old</p>
+<p>{props.children}</p>
+</div>
+)
 }
 
 export default person;
