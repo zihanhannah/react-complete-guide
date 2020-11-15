06a880e55843f6e2fbd11ccb084a682679cf4b4e
zihan zhao <zihanhannah@gmail.com>
2020-11-14 21:49:39 -0600

    practice with react

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <h1> I am react app</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <h1>Hello</h1>
+        <p>This is really working!</p>
       </div>
     );
   }
