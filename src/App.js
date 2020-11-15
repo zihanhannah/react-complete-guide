ed6771352e88f0107bc718561cc0339316a8b925
zihan zhao <zihanhannah@gmail.com>
2020-11-14 22:56:58 -0600

    using state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max" , age: 28},
+      {name:"Manu", age: 30},
+      {name:"Stephanie", age:26}
+  
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,10 +24,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
+
+
         <h1>Hello</h1>
         <p>This is really working!</p>
-        <Person name="max" age = "29" />
-        <Person name = "Manu" age = "30"> My hobbies: racing </Person>
+        {/* <Person name="max" age = "29" />
+        <Person name = "Manu" age = "30"> My hobbies: racing </Person> */}
+
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        
       </div>
     );
   }
