dee23692e5fc046e4c82b6e607ac9f8ea746fe17
zihan zhao <zihanhannah@gmail.com>
2020-11-15 22:26:21 -0600

    add the toggleHandler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 // import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -8,42 +8,50 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: "Max" , age: 28},
-      {name:"Manu", age: 30},
-      {name:"Stephanie", age:26}
-  
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 30 },
+      { name: "Stephanie", age: 26 }
+
     ],
 
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
     // console.log('was clicked');
-    this.setState({persons: [
-      {name: newName , age: 28},
-      {name:"Manu", age: 30},
-      {name:"Stephanie", age:26}
-    ]
-  })
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 30 },
+        { name: "Stephanie", age: 26 }
+      ]
+    })
   }
 
-  nameChangedHandler = (event)=> {
-    this.setState({persons: [
-      {name: "Max", age: 28},
-      {name:event.target.value, age: 30},
-      {name:"Stephanie", age:26}
-    ]
-  })
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 30 },
+        { name: "Stephanie", age: 26 }
+      ]
+    })
   }
 
-  render (){
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow});
+  }
+
+  render() {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-      }
+    }
     return (
       <div className="App">
         {/* <h1> I am react app</h1>
@@ -61,24 +69,34 @@ class App extends Component {
         {/* <Person name="max" age = "29" />
         <Person name = "Manu" age = "30"> My hobbies: racing </Person> */}
 
-        <button 
+        {/* <button 
           style={style}
-          onClick={()=>this.switchNameHandler("Maxim")}>Switch Name</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}>
+          onClick={()=>this.switchNameHandler("Maxim")}>Switch Name</button> */}
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {
+          this.state.showPersons ?
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age}>
+              </Person>
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'ZZ')}
+                changed={this.nameChangedHandler}> My hobbies: Racing
         </Person>
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this,'ZZ')}
-        changed={this.nameChangedHandler}> My hobbies: Racing
-        </Person>
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}>
-        </Person>
-        
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age}>
+              </Person>
+            </div> : null
+
+        }
+
+
       </div>
     );
   }
@@ -96,9 +114,9 @@ export default App;
 //   });
 
 //   const [otherState,setOtherState] = useState('some other value');
-  
+
 //   console.log(personsState, otherState);
-  
+
 //   const switchNameHandler = ()=>{
 //     setPersonsState({
 //       persons: [
