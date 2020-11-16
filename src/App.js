131253b76d65a8d7aeadaf1dca2d56c9a02e3de7
zihan zhao <zihanhannah@gmail.com>
2020-11-15 23:37:21 -0600

    using map to output the element

--- src/App.js
+++ src/App.js
@@ -59,22 +59,28 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}>
-          </Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'ZZ')}
-            changed={this.nameChangedHandler}> My hobbies: Racing
-        </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}>
-          </Person>
+          {this.state.persons.map(person => {
+            return <Person name={person.name}
+                            age={person.age}/>
+          })}
         </div>
-      )
+        // <div>
+        //   <Person
+        //     name={this.state.persons[0].name}
+        //     age={this.state.persons[0].age}>
+        //   </Person>
+        //   <Person
+        //     name={this.state.persons[1].name}
+        //     age={this.state.persons[1].age}
+        //     click={this.switchNameHandler.bind(this, 'ZZ')}
+        //     changed={this.nameChangedHandler}> My hobbies: Racing
+        // </Person>
+        //   <Person
+        //     name={this.state.persons[2].name}
+        //     age={this.state.persons[2].age}>
+        //   </Person>
+        // </div>
+      );
     }
     return (
       <div className="App">
