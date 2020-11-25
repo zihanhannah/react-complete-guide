9fae5781107e11914241d249dbb1fae37e92f599
zihan zhao <zihanhannah@gmail.com>
2020-11-24 23:44:29 -0600

    modify the Persons component to PureComponent to update the changes in persons

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,4 +1,4 @@
-import React , {Component} from 'react';
+import React , {PureComponent} from 'react';
 import Person from './Person/Person';
 // const persons = (props) => {
 //     console.log('[Persons.js] rendering...');
@@ -13,21 +13,24 @@ import Person from './Person/Person';
 // };
 // export default persons;
 
-class Persons extends Component{
+class Persons extends PureComponent{
     // static getDerivedStateFromProps(props,state){
     //     console.log('[Persons.js] getDerivedStateFromProps');
     //     return state;
     // }
    
-    shouldComponentUpdate(nextProps, nextState){
-        console.log('[Persons.js] shouldComponentUpdate');
-        if(nextProps.persons !== this.props.persons){
-            return true;
-          } else {
-            return false;
-          }
+    //can simply use PureComponent to update all the changes
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log('[Persons.js] shouldComponentUpdate');
+    //     if(nextProps.persons !== this.props.persons || 
+    //        nextProps.changed !== this.props.changed || 
+    //        nextProps.clicked !== this.props.clicked){
+    //         return true;
+    //       } else {
+    //         return false;
+    //       }
         
-    }
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState){
         console.log('[Persons.js] getSnapshotBeforeUpdate');
